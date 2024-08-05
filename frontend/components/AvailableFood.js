ebcfd2625b594e7260a36f8d928514a4b1f152ed
yarmajnr <yarmalight@yahoo.com>
2024-08-06 00:13:26 +0100

    Update AvailableFood.js

--- frontend/components/AvailableFood.js
+++ frontend/components/AvailableFood.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+const AvailableFood = ({ foods }) => (
+    <div>
+        <h1>Available Foods</h1>
+        <ul>
+            {foods.map(food => (
+                <li key={food.id}>{food.name} - ₦{food.price}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+export default AvailableFood;
 
