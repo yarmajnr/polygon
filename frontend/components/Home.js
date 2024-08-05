7d141bf979e1738661ffe292c9c8496836e6f3c8
yarmajnr <yarmalight@yahoo.com>
2024-08-06 00:12:36 +0100

    Update Home.js

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,1 +1,10 @@
+import React from 'react';
 
+const Home = () => (
+    <div>
+        <h1>Welcome to Polygon Restaurant</h1>
+        <p>Your one-stop shop for delicious meals.</p>
+    </div>
+);
+
+export default Home;
