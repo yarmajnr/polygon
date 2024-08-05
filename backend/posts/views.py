a11597dd3891eda26b823283b7f0df8e19f47dd9
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:27:12 +0100

    Update views.py

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,25 +1,7 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
-from django.contrib.auth.models import User
 
-class PostViewSet(viewsets.ViewSet):
-    def list(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        user = request.user
-        food_item = request.data.get('food_item')
-        price = request.data.get('price')
-        post = Post.objects.create(user=user, food_item=food_item, price=price)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        post = Post.objects.get(pk=pk)
-        post.delete()
-        return Response(status=204)
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
