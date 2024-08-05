3e10facfcb412e3b11eb98044326f14c90221dde
yarmajnr <yarmalight@yahoo.com>
2024-08-06 00:13:00 +0100

    Update Wallet.js

--- frontend/components/Wallet.js
+++ frontend/components/Wallet.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const Wallet = ({ balance }) => (
+    <div>
+        <h1>Your Wallet</h1>
+        <p>Balance: ₦{balance}</p>
+        <a href="https://paystack.com/pay/polygon">Fund Wallet</a>
+    </div>
+);
+
+export default Wallet;
 
