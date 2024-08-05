88581d19d9e797e1908f9e70a6d6794862e80522
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:23:14 +0100

    Update serializers.py

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,8 +1,7 @@
-# backend/orders/serializers.py
 from rest_framework import serializers
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('id', 'user', 'food_item', 'quantity', 'status', 'created_at')
+        fields = ['id', 'user', 'food_item', 'price', 'created_at']
