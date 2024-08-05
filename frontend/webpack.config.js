0688d38648816a47a58964ad09f55779760f7094
yarmajnr <yarmalight@yahoo.com>
2024-08-06 00:38:07 +0100

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,1 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+module.exports = {
+    entry: './frontend/components/index.js',  // Entry point for the application
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(), // Cleans the output directory before each build
+        new HtmlWebpackPlugin({
+            template: './index.html', // Template file for the HTML
+            filename: 'index.html',
+            favicon: './frontend/public/favicon.ico' // Favicon file
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        historyApiFallback: true
+    }
+};
