e437b7fe5d1eb6f24c0cf7567e04ee973df24f39
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:25:53 +0100

    Update models.py

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,14 +1,8 @@
 from django.db import models
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
+from accounts.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            "posts", {"type": "post_message", "message": {"title": self.title, "content": self.content}}
-        )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
