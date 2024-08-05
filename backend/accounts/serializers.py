5c31c7a03353ea536304480bf92c81093564d4d3
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:21:05 +0100

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,8 +1,7 @@
-# backend/accounts/serializers.py
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+        fields = ['id', 'username', 'email', 'wallet_balance']
