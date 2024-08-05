0245e1c152c8e23e3e92f6a1da56fddd7ef20042
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:24:26 +0100

    Update views.py

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,21 +1,7 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
-from django.contrib.auth.models import User
 
-class OrderViewSet(viewsets.ViewSet):
-    def list(self, request):
-        user = request.user
-        orders = Order.objects.filter(user=user)
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        user = request.user
-        food_item = request.data.get('food_item')
-        price = request.data.get('price')
-        order = Order.objects.create(user=user, food_item=food_item, price=price)
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
+class OrderListView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
