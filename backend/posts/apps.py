e0983ac79d579e07f6d13f311c922290e359dd80
yarmajnr <yarmalight@yahoo.com>
2024-08-05 23:25:27 +0100

    Update apps.py

--- backend/posts/apps.py
+++ backend/posts/apps.py
@@ -1,7 +1,4 @@
-# backend/posts/apps.py
 from django.apps import AppConfig
 
 class PostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
-    verbose_name = 'Posts'
